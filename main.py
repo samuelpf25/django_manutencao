39f5332644b1173ea1a2aec3ab50f8373165e2bb
Samuel de Paula Faria <77171431+samuelpf25@users.noreply.github.com>
2024-09-02 15:14:57 -0300

    Update main.py

--- main.py
+++ main.py
@@ -20,8 +20,22 @@ st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
 # 1) DECLARAÇÃO DE VARIÁVEIS GLOBAIS ####################################################################################
 scope = ['https://spreadsheets.google.com/feeds']
-k = "456"
-creds = ServiceAccountCredentials.from_json_keyfile_name("controle.json", scope)
+k = st.secrets["senha"]
+json = {
+    "type": st.secrets["type"],
+    "project_id": st.secrets["project_id"],
+    "private_key_id": st.secrets["project_id"],
+    "private_key": st.secrets["private_key"],
+    "client_email": st.secrets["client_email"],
+    "client_id": st.secrets["client_id"],
+    "auth_uri": st.secrets["auth_uri"],
+    "token_uri": st.secrets["token_uri"],
+    "auth_provider_x509_cert_url": st.secrets["auth_provider_x509_cert_url"],
+    "client_x509_cert_url": st.secrets["client_x509_cert_url"],
+    "universe_domain": st.secrets["universe_domain"],
+}
+
+creds = ServiceAccountCredentials.from_json_keyfile_dict(json, scope)
 
 cliente = gspread.authorize(creds)
 
